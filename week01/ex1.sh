c61962b57994232c758615bb1179729cfca39b60
Anton Kudryavtsev <a.kudryavtsev@innopolis.university>
2023-09-11 08:09:41 +0000

    fix: change relative path to absolute from home

--- week01/ex1.sh
+++ week01/ex1.sh
@@ -1,1 +1,1 @@
-ls -1 /usr/bin/*gcc* | sort -r | head -n 5 > ex1.txt
+ls -1 /usr/bin/*gcc* | sort -r | head -n 5 > ~/week01/ex1.txt
--- week01/ex2.sh
+++ week01/ex2.sh
@@ -1,1 +1,1 @@
-history | tail -n 10 > ex2.txt
+history | tail -n 10 > ~/week01/ex2.txt
